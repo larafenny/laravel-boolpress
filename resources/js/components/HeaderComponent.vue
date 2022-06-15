354321bc47184030d4b74544b2a0aee1dc9a2ef3
larafenny <lara.filippone@gmail.com>
2022-06-15 23:44:48 +0200

    created HeaderComponent and imported in HomeComponent

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <header>HEADER</header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent'
+    }
+</script>
--- resources/js/pages/HomeComponent.vue
+++ resources/js/pages/HomeComponent.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>HomeComponent</div>
+<div>
+    <HeaderComponent/>
+  <div>HOME</div>
+</div>
+
 </template>
 
 <script>
+
+import HeaderComponent from '../components/HeaderComponent.vue'
 export default {
-    name: 'HomeComponent'
+    name: 'HomeComponent',
+    components: {HeaderComponent}
 
 }
 
